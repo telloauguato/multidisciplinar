208c25188089bdb2b2b81da2d18c7926695725fe
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-10-31 16:57:37 +0000

    og

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -5,13 +5,12 @@ export function GET(request: Request) {
   let title = url.searchParams.get('title') || 'Multidisciplinar'
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
-            {title}
-          </h2>
-        </div>
+      <div tw="relative flex h-screen w-full flex-col items-center justify-center bg-black text-white">
+        <h1 tw="text-wrap p-12 text-center text-2xl font-semibold tracking-tighter z-10">{title}</h1>
+        <p tw="font-mono text-2xl z-10">Multidisciplina</p>
       </div>
+
+
     ),
     {
       width: 1200,
