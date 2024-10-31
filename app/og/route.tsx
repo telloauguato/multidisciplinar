d887305fda3e7bc9dd6dec901bd3f3c07dcdc51b
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-10-31 17:02:56 +0000

    od

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -6,7 +6,29 @@ export function GET(request: Request) {
   return new ImageResponse(
     (
       <div tw="relative flex h-screen w-full flex-col items-center justify-center bg-black text-white">
-        <h1 tw="text-wrap p-12 text-center text-2xl font-semibold tracking-tighter z-10">{title}</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="400"
+          height="400"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          style={{
+            position: 'absolute',
+            top: '25%',
+            left: '75%',
+            transform: 'translate(-50%, -50%)',
+            opacity: 0.1,
+          }}
+        >
+          <path d="M12 6v12" />
+          <path d="M17.196 9 6.804 15" />
+          <path d="m6.804 9 10.392 6" />
+        </svg>
+        <h1 tw="text-wrap p-12 text-center text-6xl font-semibold tracking-tighter z-10">{title}</h1>
         <p tw="font-mono text-2xl z-10">Multidisciplina</p>
       </div>
 
