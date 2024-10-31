a8aafdc477d07d629e679b1ff0d75bf46418c832
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-10-31 17:16:05 +0000

    og

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -5,34 +5,19 @@ export function GET(request: Request) {
   let title = url.searchParams.get('title') || 'Multidisciplinar'
   return new ImageResponse(
     (
-      <div tw="relative flex h-screen w-full flex-col items-center justify-center bg-black text-white">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="400"
-          height="400"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          style={{
-            position: 'absolute',
-            top: '25%',
-            left: '75%',
-            transform: 'translate(-50%, -50%)',
-            opacity: 0.1,
-          }}
-        >
-          <path d="M12 6v12" />
-          <path d="M17.196 9 6.804 15" />
-          <path d="m6.804 9 10.392 6" />
-        </svg>
-        <h1 tw="text-wrap p-12 text-center text-6xl font-semibold tracking-tighter z-10">{title}</h1>
-        <p tw="font-mono text-2xl z-10">Multidisciplina</p>
+      <div tw="flex flex-col h-screen">
+        <div tw="mx-auto flex h-full w-full">
+          <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M12 6v12" />
+            <path d="M17.196 9 6.804 15" />
+            <path d="m6.804 9 10.392 6" />
+          </svg>
+          <div>
+            <h1 tw="text-wrap p-12 text-5xl font-semibold tracking-tighter">{title}</h1>
+          </div>
+        </div>
+        <p tw="z-10 font-mono text-2xl p-4 text-center">Multidisciplina</p>
       </div>
-
-
     ),
     {
       width: 1200,
